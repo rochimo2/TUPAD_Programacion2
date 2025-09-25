365db84264c108943c16e85241733cc38133a861
Rocio <32850111+rochimo2@users.noreply.github.com>
2025-09-25 11:30:39 -0300

    Des-comentando el codigo que no necesita estar comentado

--- Tp unidad 3/Estudiantes/src/Main.java
+++ Tp unidad 3/Estudiantes/src/Main.java
@@ -3,63 +3,63 @@
 public class Main {
     public static void main(String[] args) {
 
-//        //Estudiante
-//        Estudiante estudiante = new Estudiante();
-//        estudiante.nombre = "Rocio Moyano";
-//        estudiante.edad = 38;
-//        estudiante.curso = "Java";
-//        estudiante.calificacion = 5;
-//        estudiante.mostrarInfo();
-//
-//        System.out.println("Subir calificación a: " + estudiante.subirCalificacion(5));
-//        estudiante.mostrarInfo();
-//
-//        System.out.println("Bajar calificación a: " + estudiante.bajarCalificacion(3));
-//        estudiante.mostrarInfo();
-//
-//        // Mascota
-//        Mascota mascota = new Mascota();
-//        mascota.nombre = "Firulais";
-//        mascota.especie = "Perro";
-//        mascota.edad = 3;
-//        mascota.mostrarInfo();
-//
-//        mascota.cumplirAnios();
-//        System.out.println("Después de cumplir años:");
-//        mascota.mostrarInfo();
+        //Estudiante
+        Estudiante estudiante = new Estudiante();
+        estudiante.nombre = "Rocio Moyano";
+        estudiante.edad = 38;
+        estudiante.curso = "Java";
+        estudiante.calificacion = 5;
+        estudiante.mostrarInfo();
 
-//        // Libro
-//        Libro libro = new Libro();
-//        libro.setAnioPublicacion(-2);
-//        libro.getAnioPublicacion();
-//        libro.setAnioPublicacion(2025);
-//        libro.getAnioPublicacion();
+        System.out.println("Subir calificación a: " + estudiante.subirCalificacion(5));
+        estudiante.mostrarInfo();
 
-//        // Gallinas
-//
-//        Gallina cocoroco = new Gallina();
-//        cocoroco.idGallina = 1;
-//        Gallina tita = new Gallina();
-//        tita.idGallina = 2;
-//
-//        cocoroco.ponerHuevo();
-//        cocoroco.envejecer();
-//        cocoroco.ponerHuevo();
-//        cocoroco.envejecer();
-//        cocoroco.envejecer();
-//
-//        tita.ponerHuevo();
-//        tita.ponerHuevo();
-//        tita.ponerHuevo();
-//        tita.envejecer();
-//        tita.ponerHuevo();
-//        tita.envejecer();
-//
-//        cocoroco.mostrarEstado();
-//
-//        tita.mostrarEstado();
+        System.out.println("Bajar calificación a: " + estudiante.bajarCalificacion(3));
+        estudiante.mostrarInfo();
 
-        // Nave espacial
+        // Mascota
+        Mascota mascota = new Mascota();
+        mascota.nombre = "Firulais";
+        mascota.especie = "Perro";
+        mascota.edad = 3;
+        mascota.mostrarInfo();
+
+        mascota.cumplirAnios();
+        System.out.println("Después de cumplir años:");
+        mascota.mostrarInfo();
+
+        // Libro
+        Libro libro = new Libro();
+        libro.setAnioPublicacion(-2);
+        libro.getAnioPublicacion();
+        libro.setAnioPublicacion(2025);
+        libro.getAnioPublicacion();
+
+        // Gallinas
+
+        Gallina cocoroco = new Gallina();
+        cocoroco.idGallina = 1;
+        Gallina tita = new Gallina();
+        tita.idGallina = 2;
+
+        cocoroco.ponerHuevo();
+        cocoroco.envejecer();
+        cocoroco.ponerHuevo();
+        cocoroco.envejecer();
+        cocoroco.envejecer();
+
+        tita.ponerHuevo();
+        tita.ponerHuevo();
+        tita.ponerHuevo();
+        tita.envejecer();
+        tita.ponerHuevo();
+        tita.envejecer();
+
+        cocoroco.mostrarEstado();
+
+        tita.mostrarEstado();
+
+         //Nave espacial
 
         NaveEspacial nabucodonosor = new NaveEspacial();
         nabucodonosor.nombre = "Nabucodonosor";
